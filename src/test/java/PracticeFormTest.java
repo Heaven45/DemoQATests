5e689d689d41e3117a782b53c8762d749f6decb0
s.fedulov <s.fedulov@corp.vk.com>
2021-09-11 22:09:14 +0300

    Add Registration Page PageObject and Calendar component

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,6 +1,10 @@
+import pages.RegistrationPage;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -13,9 +17,29 @@ public class PracticeFormTest {
     String email = "test@test.com";
     String gender = "Male";
     String mobile = "1234567890";
+    String day = "07";
+    String month = "July";
+    String year = "1984";
+    String fullDate = "07 July,1984";
     String subjects = "Computer Science";
+    String pathToImage = "images/test.png";
     String currentAddress = "Moscow, Pushkina 12, apartment 345";
     String stateAndCity = "NCR Delhi";
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    // Мапа для метода .checkResult
+    public Map<String, String> valuesToCheck = Map.of(
+            "Student Name", fullName,
+            "Student Email", email,
+            "Gender", gender,
+            "Mobile", mobile,
+            "Date of Birth", fullDate,
+            "Subjects", subjects,
+            "Hobbies", "Music",
+            "Picture", "test.png",
+            "Address", currentAddress,
+            "State and City", stateAndCity);
 
     @BeforeAll
     static void beforeAll() {
@@ -24,36 +48,33 @@ public class PracticeFormTest {
 
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $("[for=gender-radio-1]").click();
-        $("#userNumber").setValue(mobile);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("April");
-        $(".react-datepicker__year-select").selectOption("1984");
-        $x("//div[contains(@aria-label, \"Monday, April 9th, 1984\")]").click();
-        $("#subjectsInput").setValue(subjects).pressEnter();
-        $("[for=hobbies-checkbox-3]").click();
-        $("#uploadPicture").uploadFromClasspath("images/test.png");
-        $("#currentAddress").setValue(currentAddress);
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
 
-        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(fullName));
-        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
-        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
-        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(mobile));
-        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("09 April,1984"));
-        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subjects));
-        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Music"));
-        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("test.png"));
-        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
-        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(stateAndCity));
+        registrationPage
+                .openPage()
+                .typeFirstName(firstName)
+                .typeLastName(lastName)
+                .typeEmail(email)
+                .choseGender()
+                .typeNumber(mobile)
+                .pickDate(day, month, year)
+                .typeSubject(subjects)
+                .choseHobbies()
+                .uploadPicture(pathToImage)
+                .typeAddress(currentAddress)
+                .choseStateAndCity("NCR", "Delhi")
+                .submit()
+                .checkResult(valuesToCheck);
+
+//        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+//        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(fullName));
+//        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
+//        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
+//        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(mobile));
+//        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("09 April,1984"));
+//        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subjects));
+//        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Music"));
+//        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("test.png"));
+//        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
+//        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(stateAndCity));
     }
 }
