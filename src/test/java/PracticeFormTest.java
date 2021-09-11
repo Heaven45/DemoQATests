c61adc3a73b7d0a4f9bf6e0ed935baaa1f0c981f
s.fedulov <s.fedulov@corp.vk.com>
2021-09-11 22:53:42 +0300

    Add Faker

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import pages.RegistrationPage;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
@@ -5,25 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
-
 public class PracticeFormTest {
+    Faker faker = new Faker();
 
-    String firstName = "Ivan";
-    String lastName = "Ivanov";
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
     String fullName = String.format("%s %s", firstName, lastName);
-    String email = "test@test.com";
+    String email = faker.internet().emailAddress();
     String gender = "Male";
-    String mobile = "1234567890";
+    String mobile = faker.phoneNumber().subscriberNumber(10);
     String day = "07";
     String month = "July";
     String year = "1984";
     String fullDate = "07 July,1984";
     String subjects = "Computer Science";
     String pathToImage = "images/test.png";
-    String currentAddress = "Moscow, Pushkina 12, apartment 345";
+    String currentAddress = faker.address().fullAddress();
     String stateAndCity = "NCR Delhi";
 
     RegistrationPage registrationPage = new RegistrationPage();
@@ -64,17 +62,5 @@ public class PracticeFormTest {
                 .choseStateAndCity("NCR", "Delhi")
                 .submit()
                 .checkResult(valuesToCheck);
-
-//        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-//        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text(fullName));
-//        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text(email));
-//        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text(gender));
-//        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text(mobile));
-//        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("09 April,1984"));
-//        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text(subjects));
-//        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Music"));
-//        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("test.png"));
-//        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
-//        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text(stateAndCity));
     }
 }
